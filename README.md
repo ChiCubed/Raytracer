dac1a6cc7bd5c4ee78442116aa3b4beeb6ea3a3e
Albert Smith <albert.chejin.edward.smith@outlook.com>
2017-03-05 22:44:12 +0800

    Add some test code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Raytracer
 A raytracer.
 
-Dependencies: (Boost)[http://www.boost.org/]
+Dependencies: [Boost](http://www.boost.org) >= 1.6.2 (needs QVM installed)
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <boost/qvm/all.hpp>
+
+
+int main(int argc, char* argv[]) {
+    boost::qvm::vec<float,3> v = {0,0,7};
+
+    std::cout << "test" << std::endl;
+
+    return 0;
+}
